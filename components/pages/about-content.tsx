6115c35502a19f7735f601288923745088072d37
franklin <franklin.tejadag@gmail.com>
2025-09-30 19:13:15 -0700

    Update: about con image

--- components/pages/about-content.tsx
+++ components/pages/about-content.tsx
@@ -126,7 +126,7 @@ const AboutContent = () => {
         >
           <div className="relative aspect-[4/5] rounded-2xl overflow-hidden shadow-2xl">
             <Image
-              src="https://images.unsplash.com/photo-1573408301185-9146fe634ad0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+              src="https://i.postimg.cc/SsjDmvmc/julia.png"
               alt="Julia creating handmade crafts with love"
               fill
               className="object-cover"
